9d885071f493a714eb22aeb91bb9c83d72775425
Tiago <tiago.kise@hotmail.com>
2020-12-23 18:11:00 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Card from './Components/Card/Card';
 import Section from './Components/Section/Section';
-import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -11,7 +9,6 @@ function App() {
       <Header/>
       <Card/>
       <Section/>
-      {/* <Footer/> */}
 
     </div>
   );
